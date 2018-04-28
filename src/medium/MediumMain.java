25f9fa9b557085cb19db30192b88af5643dfdb3d
somyungsub <gkdldy5@naver.com>
2018-04-28 21:19:33 +0900

    중위->후위변환처리 완성

--- src/medium/MediumMain.java
+++ src/medium/MediumMain.java
@@ -1,9 +1,13 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class MediumMain {
+
+    private static Map<String, Integer> operObj = new HashMap<>();
 
     public static void main(String[] args) {
         /*
@@ -12,83 +16,105 @@ public class MediumMain {
             : 1 * ( 2 * ( 5 - 4 ) * ( 6 * 7 ) ) $
          */
 
-        // 입력1 : 1 * 2 - ( 3 + 4 ) / 5 $
-        System.out.println(convertRearToMedium());
+        // 연산 우선순위 정의
+        operObj.put("+", 1);
+        operObj.put("-", 1);
+        operObj.put("*", 2);
+        operObj.put("/", 2);
 
-        // 입력2 : 1 * ( 2 * ( 5 - 4 ) * ( 6 * 7 ) ) $
-        System.out.println(convertRearToMedium());
+        // 입력1 : 1 * 2 - ( 3 + 4 ) / 5 $
+        System.out.println(convertMediumToRear());
+
+        // 입력2 : 1 * ( 2 + ( 5 - 4 ) * ( 6 + 7 ) ) $
+        System.out.println(convertMediumToRear());
     }
 
     /*
         중위->후위로 변환 한다.
      */
-    private static String convertRearToMedium() {
+    private static String convertMediumToRear() {
         // 1. 입력 값을 받는다.
-        String str = getString();
+        Scanner sc = new Scanner(System.in);    // 입력을 위한 스캐너 객체 생성 및 인풋스트림형성
+        String s = sc.nextLine();               // 입력받기
+        String str = s.replace("$",""); // 입력 값 반환 및 $는 empty(비어있는) 값으로 변환 시킨 후 반환
 
         // 2. 받은 입력 값을 split메서드를 활용하여 String배열을 만든다.
         String[] strArr = str.split(" ");
 
-        //
-        calc(strArr);
+        // 중위 -> 후위로 변환
+        convert(strArr);
         return "";
     }
 
     /*
-        1. 입력 값을 받는 메서드
+        중위식을 후위식으로 변환하기
+        1. 숫자와 연산자 2개를 담는 Stack 구조를 활용
      */
-    private static String getString(){
-        Scanner sc = new Scanner(System.in);    // 입력을 위한 스캐너 객체 생성 및 인풋스트림형성
-        String s = sc.nextLine();               // 입력받기
-        sc.close();                             // 입력 받은 후 스트림 닫기
-        return s.replace("$","");   // 입력 값 반환 및 $는 empty(비어있는) 값으로 변환 시킨 후 반환
-    }
+    private static void convert(String[] strArr) {
+        Stack<String> numberStack = new Stack<>();  // 숫자를 담는 스택
+        Stack<String> operStack = new Stack<>();    // 연산자를 담는 스택
+        String temp = "";   // ()연산을 처리하기 위한 변수
 
-    private static void calc(String[] strArr) {
-        Stack<String> operStack = new Stack<>();
-        Stack<String> numStack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
+        // 중위식 순회
         for (String str : strArr) {
 
-            if (str.matches("[0-9]")) {
-                numStack.push(str);
+            // 숫자스택이 적어도 2개이상 인 경우 and 현재str변수가 연산자인 경우에 실행
+            if (numberStack.size() >= 2 && !(str.matches(".*[0-9].*"))) {
+
+                // 연산자 우선순위 판단을 위해 직전에 Stack쌓인 데이터 pop
+                String operPre = operStack.pop();
+
+                // 앞의 연산자가 현재연산자보다 우선순위가 같거나 크면 변환 실행
+                if ((")").equals(str)  || !("(").equals(str) && operObj.get(operPre) >= operObj.get(str)) {
+
+                    // ()가 있는 경우 처리
+                    if("(".equals(temp)){
+                        temp = "";  // (가 있는걸 확인했으므로 다시 초기화
+                        operStack.push(operPre); // Stack 재 저장
+                    }else{
+                        // 나머지는 변환 연산 실행
+                        String pp = calculate(numberStack, operPre);
+
+                        // 변환 후 변환된 문자열 저장
+                        numberStack.push(pp);
+                    }
+                }
+                else {
+                    operStack.push(operPre); // Stack 재 저장
+                }
             }
+
+            // 숫자인 경우 숫자 스택에 저장
+            if (str.matches(".*[0-9].*")) {
+                numberStack.push(str);
+            }
+            // 연산자 인 경우 연산자 스택에 저장
             else{
+                // ()는 저장에서 제외
+                if ("()".contains(str)) {
+                    temp = str;
+                    continue;
+                }
                 operStack.push(str);
             }
-
-            if (numStack.size() >= 2 && operStack.size() >= 1) {
-                String s1 = numStack.pop();
-                String s2 = numStack.pop();
-                sb.append(s2).append(" ").append(s1).append(" ").append(operStack.pop()).append(" ");
-            }
-
-            if (str.equals("(")) {
-
-            }
         }
+
+        // 숫자 Stack이 1개일 때 까지 변환 연산
+        while (numberStack.size() > 1) {
+            numberStack.push(calculate(numberStack, operStack.pop()));
+        }
+
+        // 최종적으로 후위식으로 변환된 문자열 반환
+        System.out.println(numberStack.pop().replace("  ", " "));
     }
 
-    private static void perio(Stack<String> operStack) {
-
-        if (operStack.size() > 1) {
-            String perio1 = "+,-";
-            String perio2 = "*,/";
-            String s1 = operStack.pop();
-            String s2 = operStack.pop();
-            int num1 = perio1.contains(s1) ? 1 : 2;
-            int num2 = perio2.contains(s2) ? 2 : 1;
-
-            // 순서교체
-            if (num1 <= num2) {
-                operStack.push(s2);
-                operStack.push(s1);
-            }
-            // 그대로 유지
-            else{
-                operStack.push(s1);
-                operStack.push(s2);
-            }
-        }
+    /*
+        후위식 변환 연산
+     */
+    private static String calculate(Stack<String> numberStack, String operPre) {
+        StringBuilder sb = new StringBuilder();
+        String pop1 = numberStack.pop();
+        String pop2 = numberStack.pop();
+        return sb.append(pop2).append(" ").append(pop1).append(" ").append(operPre).append(" ").toString();
     }
 }
