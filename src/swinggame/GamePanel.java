36f91a7331a19a80c9a79c4453c084712478aa02
somyungsub <gkdldy5@naver.com>
2018-05-30 15:04:09 +0900

    Add : 숫자맞추기 로직 추가

--- src/swinggame/GamePanel.java
+++ src/swinggame/GamePanel.java
@@ -7,6 +7,9 @@ import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
+    private JLabel[] jLabel;
+    private JLabel labelText;
+
     public GamePanel(){
         JFrame container = new JFrame();
         container.setSize(600, 300);
@@ -15,53 +18,44 @@ public class GamePanel extends JPanel {
         container.setTitle("숫자맞추기");
 
         JPanel panel = new JPanel();
+//        panel.setFocusable(true);
+//        panel.requestFocus();
 
-        JLabel label1 = new JLabel("0", JLabel.CENTER);
-        JLabel label2 = new JLabel("0", JLabel.CENTER);
-        JLabel label3 = new JLabel("0",JLabel.CENTER);
-        JLabel label4 = new JLabel("시작합니다.",JLabel.CENTER);
-
-        label1.setBackground(new Color(Color.HSBtoRGB(100, 100, 100)));
-        label2.setBackground(Color.MAGENTA);
-        label3.setBackground(Color.MAGENTA);
-        label1.setSize(100,50);
-        label2.setSize(100,50);
-        label3.setSize(100,50);
-
-//        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
-//        flowLayout.addLayoutComponent("label1", label1);
-//        flowLayout.addLayoutComponent("label2", label2);
-//        flowLayout.addLayoutComponent("label3", label3);
-
-        panel.add(label1);
-        panel.add(label2);
-        panel.add(label3);
-//        panel.add(label4);
-
+        jLabel = new JLabel[3];
+        for (int i = 0; i < jLabel.length; i++) {
+            jLabel[i] = new JLabel("0", JLabel.CENTER);
+            panel.add(jLabel[i]);
+        }
+        labelText = new JLabel("시작합니다.",JLabel.CENTER);
+        panel.add(labelText);
         container.add(panel);
         container.setVisible(true);
 
-        int x = (int) (Math.random() * 5);
-
-
-        panel.addKeyListener(new KeyListener() {
+        container.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
-
             }
 
             @Override
             public void keyPressed(KeyEvent e) {
                 if(e.getKeyChar() == KeyEvent.VK_ENTER) {
+                    for (int i = 0; i < jLabel.length; i++) {
+                        int x = (int) (Math.random() * 5);
+                        jLabel[i].setText(String.valueOf(x));
+                    }
 
-
+                    if (jLabel[0].getText().equals(jLabel[1].getText())
+                            && jLabel[0].getText().equals(jLabel[2].getText())
+                            && jLabel[1].getText().equals(jLabel[2].getText())) {
+                        labelText.setText("축하합니다!!");
+                    } else {
+                        labelText.setText("아쉽군요");
+                    }
 
                 }
             }
-
             @Override
             public void keyReleased(KeyEvent e) {
-
             }
         });
     }
