e58f5d9c6b100f9d0746a30e99e4234c9a330a31
somyungsub <gkdldy5@naver.com>
2018-05-30 17:55:09 +0900

    Add : 스윙 클릭시 랜덤 위치 설정 추가

---
+++ src/swingclick/ClickLabel.java
@@ -0,0 +1,61 @@
+package swingclick;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ClickLabel extends JFrame {
+
+    // 실행
+    public static void main(String[] args) {
+        new ClickLabel();
+    }
+
+    private JLabel cLabel;
+    public ClickLabel() {
+
+        setTitle("클릭응용프로그래밍");      // 제목설정
+        setVisible(true);                    // 보여지기 기본 설정
+        setSize(500, 500);  // 창 크기 설정
+        setLayout(null);                    // 레이아웃 설정 -> null
+
+        cLabel = new JLabel("C");    // C가 쓰이는 레이블 생성 및 텍스트 설정
+        cLabel.setBounds(100,100,10,10);    // 위치설정
+
+        // 라벨을 컨텐트팬에 추가
+        add(cLabel);
+
+        // 마우스 이벤트 등록
+        addMouseListener(new MouseListener() {
+
+            // 마우스 클릭시 이벤트 처리
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // 클릭위치 와 C레이블 위치 비교 후 같으면 랜덤으로 다른 곳으로 위치 설정
+                if(cLabel.getX() + 5 < e.getX() && cLabel.getX() + 20 > e.getX()
+                        && cLabel.getY() + 28 < e.getY() && cLabel.getY() + 45 > e.getY()){
+                    System.out.println("!!!!!!!!");
+                    cLabel.setLocation((int)(Math.random() * 450),(int)(Math.random() * 450));
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        });
+    }
+
+}
+
