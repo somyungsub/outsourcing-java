06ea70b2001d16f83d07eb4a102531628f07d030
somyungsub <gkdldy5@naver.com>
2018-06-08 00:23:23 +0900

    Add : 간단한 성적관리 프로그램 추가

--- src/lotto/Main.java
+++ src/lotto/Main.java
@@ -2,9 +2,7 @@ package lotto;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
